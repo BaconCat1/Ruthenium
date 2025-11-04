bb007b4c77ef436b432621587deb7aa1248499ef
BaconCat__ <126538523+BaconCat1@users.noreply.github.com>
2025-11-04 16:17:11 +0800

    Initial commit

---
+++ src/main/java/org/bacon/ruthenium/Ruthenium.java
@@ -0,0 +1,10 @@
+package org.bacon.ruthenium;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Ruthenium implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
